fb6258468e2957ace278d420b71d2a689e8d1c02
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2022-12-20 21:18:08 +0700

    add custom env

--- pkg/environment/read.go
+++ pkg/environment/read.go
@@ -2,10 +2,45 @@ package environment
 
 import (
 	"fmt"
-	"github.com/peachoenixz/assessment/pkg/log"
+	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/peachoenixz/assessment/pkg/log"
 )
+
+type CustomEnv struct {
+	PORT     string
+	DATABASE string
+}
+
+func (ce CustomEnv) checkCustomEnv() bool {
+	if ce.PORT != "" && ce.DATABASE != "" {
+		return true
+	}
+	return false
+}
+
+func (ce CustomEnv) customVariableEnv() {
+	err := os.Setenv("PORT", ce.PORT)
+	if err != nil {
+		return
+	}
+	err = os.Setenv("DATABASE_URL", ce.PORT)
+	if err != nil {
+		return
+	}
+}
+
+func InitEnv() {
+	ce := CustomEnv{
+		PORT:     os.Getenv("PORT"),
+		DATABASE: os.Getenv("DATABASE_URL"),
+	}
+	ReadEnv("environment")
+	if ce.checkCustomEnv() {
+		ce.customVariableEnv()
+	}
+}
 
 func ReadEnv(filename string) string {
 	if err := godotenv.Load(fmt.Sprintf("%v.env", filename)); err != nil {
