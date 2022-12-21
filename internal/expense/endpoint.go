779ca364a1d9486498ca2c0008d4e699cf2d6104
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2022-12-22 01:05:26 +0700

    fix add expenses response

--- internal/expense/endpoint.go
+++ internal/expense/endpoint.go
@@ -1,7 +1,6 @@
 package expense
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -23,13 +22,11 @@ type Request struct {
 }
 
 type Response struct {
-	ID           int      `json:"id"`
-	Title        string   `json:"title"`
-	Amount       float64  `json:"amount"`
-	Note         string   `json:"note"`
-	Tags         []string `json:"tags"`
-	Status       string   `json:"status"`
-	ErrorMessage string   `json:"error_message"`
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount float64  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
 }
 
 func NewEndpoint(ServiceExpense ServiceUseCase) *Endpoint {
@@ -41,28 +38,19 @@ func NewEndpoint(ServiceExpense ServiceUseCase) *Endpoint {
 func (e Endpoint) AddExpense(c echo.Context) error {
 	var req Request
 	if err := c.Bind(&req); err != nil {
-		return c.JSON(http.StatusBadRequest, Response{
-			Status:       "Failed",
-			ErrorMessage: err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, 400)
 	}
-
-	fmt.Println(req.Title, req.Amount, req.Note, req.Tags)
+	
 	id, err := e.Service.AddExpense(req)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Response{
-			Status:       "Failed",
-			ErrorMessage: err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, 500)
 	}
 
-	return c.JSON(http.StatusBadRequest, Response{
-		ID:           id,
-		Title:        req.Title,
-		Note:         req.Note,
-		Amount:       req.Amount,
-		Tags:         req.Tags,
-		Status:       fmt.Sprintf("%v", http.StatusCreated),
-		ErrorMessage: "",
+	return c.JSON(http.StatusCreated, Response{
+		ID:     id,
+		Title:  req.Title,
+		Note:   req.Note,
+		Amount: req.Amount,
+		Tags:   req.Tags,
 	})
 }
