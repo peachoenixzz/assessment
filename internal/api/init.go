ff0e901126b7fc7d14171aba40437b48653691e3
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2022-12-23 04:19:13 +0700

    refactor router in api init

--- internal/api/init.go
+++ internal/api/init.go
@@ -10,17 +10,41 @@ import (
 	"github.com/peachoenixz/assessment/pkg/log"
 )
 
+type Router interface {
+	routerRead(endpoint *expense.Endpoint)
+	routerUpdate(endpoint *expense.Endpoint)
+	routerCreate(endpoint *expense.Endpoint)
+}
+
+type RouterSession struct {
+	Session *echo.Echo
+}
+
+func (r RouterSession) routerRead(endpoint *expense.Endpoint) {
+	r.Session.GET("/expenses/:id", endpoint.ViewExpenseByID)
+}
+
+func (r RouterSession) routerUpdate(endpoint *expense.Endpoint) {
+
+}
+
+func (r RouterSession) routerCreate(endpoint *expense.Endpoint) {
+	r.Session.POST("/expenses", endpoint.AddExpense)
+}
+
 func serviceRouter() {
-	e := echo.New()
+	var e RouterSession
+	e.Session = echo.New()
 	postgresDBClient := database.NewPostgres()
 	expensePostgresRepo := expense.NewPostgres(postgresDBClient.Client)
 	expenseServiceAPI := expense.NewService(expensePostgresRepo)
 	expenseEndpoint := expense.NewEndpoint(expenseServiceAPI)
-	e.POST("/expenses", expenseEndpoint.AddExpense)
-	e.GET("/expenses/:id", expenseEndpoint.ViewExpenseByID)
+	e.routerRead(expenseEndpoint)
+	e.routerCreate(expenseEndpoint)
+	e.routerRead(expenseEndpoint)
 
 	log.InfoLog("ECHO PREPARE TO START", "ECHO API")
-	log.ErrorLog(e.Start(os.Getenv("PORT")), "ECHO API")
+	log.ErrorLog(e.Session.Start(os.Getenv("PORT")), "ECHO API")
 }
 
 func EchoStart() {
