8a18e5e27141f5bf0f40df4dc71d4f7da98a9edf
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2022-12-24 09:42:36 +0700

    add unit test update expenses

--- internal/api/api_test.go
+++ internal/api/api_test.go
@@ -16,6 +16,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type response struct {
+	ID     string   `json:"id"`
+	Title  string   `json:"title"`
+	Amount float64  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
+
 func TestViewExpense(t *testing.T) {
 	// Arrange
 	e := echo.New()
@@ -82,14 +90,8 @@ func TestViewExpenseByID(t *testing.T) {
 
 func TestAddExpenses(t *testing.T) {
 	//Arrange
-	ex := struct {
-		ID     int      `json:"id"`
-		Title  string   `json:"title"`
-		Amount float64  `json:"amount"`
-		Note   string   `json:"note"`
-		Tags   []string `json:"tags"`
-	}{
-		ID:     1,
+	ex := response{
+		ID:     "1",
 		Title:  "apple bla bla",
 		Amount: 500,
 		Note:   "buy apple but no discount",
@@ -121,6 +123,48 @@ func TestAddExpenses(t *testing.T) {
 	// Assertions
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
+	}
+}
+
+func TestUpdateExpensesByID(t *testing.T) {
+	// Arrange
+	ex := response{
+		ID:     "1",
+		Title:  "Edit : apple not smoothie",
+		Amount: 150,
+		Note:   "have discount",
+		Tags:   []string{"market"},
+	}
+	jsonParam := `{"title":"Edit : apple not smoothie","amount":150,"note":"have discount","tags":["market"]}`
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPut, "/expenses", strings.NewReader(string(jsonParam)))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	newsMockRows := sqlmock.NewRows([]string{"title", "amount", "note", "tags", "id"}).
+		AddRow("Edit : apple not smoothie", 150, "have discount", pq.Array(&[]string{"market"}), 1)
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual)) //sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual)
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	mock.ExpectPrepare("UPDATE expenses SET title=$1,amount=$2,note=$3,tags=$4 WHERE id=$5 RETURNING title,amount,note,tags,id").ExpectQuery().
+		WithArgs(ex.Title, ex.Amount, ex.Note, pq.Array(&ex.Tags), ex.ID).WillReturnRows(newsMockRows)
+
+	expensePostgresRepo := expense.NewPostgresMock(db)
+	expenseServiceAPI := expense.NewService(expensePostgresRepo)
+	expenseEndpoint := expense.NewEndpoint(expenseServiceAPI)
+	c := e.NewContext(req, rec)
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+	expected := `{"id":1,"title":"Edit : apple not smoothie","amount":150,"note":"have discount","tags":["market"]}`
+	// Act
+	err = expenseEndpoint.EditExpenseByID(c)
+
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 	}
 }
